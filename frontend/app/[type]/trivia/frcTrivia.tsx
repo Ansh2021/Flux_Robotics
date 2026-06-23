b5be31c04d6c37759ff66e9c4c46fa7bf585ba11
Ansh Raghapur <anshraghapur@gmail.com>
2026-06-23 15:43:26 -0400

    devlog #8: frc trivia finally done. supabase implemented (will do the same with frcdle when statbotics is back up)

--- frontend/app/[type]/trivia/frcTrivia.tsx
+++ frontend/app/[type]/trivia/frcTrivia.tsx
@@ -1,17 +1,21 @@
-import { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import {
   Combobox,
   ComboboxButton,
   ComboboxInput,
   ComboboxOption,
   ComboboxOptions,
+  Description,
+  Field,
+  Input,
+  Label,
 } from "@headlessui/react";
 import { CheckIcon, ChevronDownIcon } from "@heroicons/react/20/solid";
 import clsx from "clsx";
 import { Bounce, toast } from "react-toastify";
 
 //Filtering options for questions
-//Core Category (general FIRST knowledge, events, awards, teams)
+//Core Category (general FIRST knowledge, events, awards, team)
 //Era (historical, current, upcoming)
 //Difficulty (easy, medium, hard)
 //Answer Type (MCQ, FRQ, True/False)
@@ -46,8 +50,12 @@ export default function FRCTrivia() {
 
   const [questionData, setQuestionData] = useState<QuestionData[]>([]);
   const [randomQuestion, setRandomQuestion] = useState<QuestionData>();
+  const [validQuestions, setValidQuestions] = useState<QuestionData[]>([]);
 
   const [fetchQuestionData, setFetchQuestionData] = useState(0);
+  const [shuffleQuestion, setShuffleQuestion] = useState(0);
+
+  const [endTrivia, setEndTrivia] = useState(false);
 
   useEffect(() => {
     setTimeout(() => {
@@ -73,9 +81,9 @@ export default function FRCTrivia() {
         const res = await fetch(
           `${process.env.NEXT_PUBLIC_API_URL}/frc/trivia/get-questions/?${urlParams.toString()}`,
         );
-        console.log(
-          `${process.env.NEXT_PUBLIC_API_URL}/frc/trivia/get-questions/?${urlParams.toString()}`,
-        );
+        // console.log(
+        //   `${process.env.NEXT_PUBLIC_API_URL}/frc/trivia/get-questions/?${urlParams.toString()}`,
+        // );
 
         if (!res.ok) {
           throw new Error(`Error Status: ${res.status}`);
@@ -83,6 +91,7 @@ export default function FRCTrivia() {
 
         const data = (await res.json()) as QuestionData[];
         setQuestionData(data);
+        setValidQuestions(data);
         console.log(data);
 
         setTriviaModalVisible(false);
@@ -120,10 +129,35 @@ export default function FRCTrivia() {
   }, [fetchQuestionData]);
 
   useEffect(() => {
-    setRandomQuestion(
-      questionData[Math.floor(Math.random() * questionData.length)],
-    );
+    const randIndex = Math.floor(Math.random() * questionData.length);
+    setRandomQuestion(questionData[randIndex]);
+    setEndTrivia(false);
+    setValidQuestions(() => {
+      const newArr = [...validQuestions];
+      newArr.splice(randIndex, 1);
+      return newArr;
+    });
   }, [questionData]);
+
+  useEffect(() => {
+    const randIndex = Math.floor(Math.random() * validQuestions.length);
+    setRandomQuestion(validQuestions[randIndex]);
+    if (validQuestions.length === 0) {
+      setEndTrivia(true);
+    }
+    setValidQuestions(() => {
+      const newArr = [...validQuestions];
+      newArr.splice(randIndex, 1);
+      return newArr;
+    });
+  }, [shuffleQuestion]);
+
+  // useEffect(() => {
+  //   if (validQuestions.length === 0) {
+  //     setEndTrivia(true);
+  //     console.log(validQuestions, validQuestions.length);
+  //   }
+  // }, [validQuestions]);
 
   const insufficientInfo = () => {
     toast.error("Please fill in every field", {
@@ -144,6 +178,8 @@ export default function FRCTrivia() {
         <FRCTriviaQuestions
           questionsVisible={questionsVisible}
           questionData={randomQuestion}
+          setShuffleQuestion={setShuffleQuestion}
+          endTrivia={endTrivia}
         />
 
         <FRCTriviaModal triviaModalVisible={triviaModalVisible}>
@@ -527,19 +563,244 @@ function FRCTriviaModal({
 function FRCTriviaQuestions({
   questionsVisible,
   questionData,
+  setShuffleQuestion,
+  endTrivia,
 }: {
   questionsVisible: boolean;
   questionData: QuestionData;
+  setShuffleQuestion: React.Dispatch<React.SetStateAction<number>>;
+  endTrivia: boolean;
 }) {
+  const [questionAnswers, setQuestionAnswers] = useState<string[]>([]);
+  const [shuffledAnswers, setShuffledAnswers] = useState<string[]>([]);
+  const [selectedLetter, setSelectedLetter] = useState("");
+  const [guessedAnswer, setGuessedAnswer] = useState("");
+  const [finalAnswer, setFinalAnswer] = useState("");
+
+  const [totalAnswered, setTotalAnswered] = useState(0);
+  const [totalCorrect, setTotalCorrect] = useState(0);
+
+  const [randomQuestionIndexes, setRandomQuestionIndexes] = useState<number[]>([
+    0, 1, 2, 3,
+  ]);
+
+  //TODO: randomize answers (correct answer shouldn't always be A)
+  useEffect(() => {
+    const answer_: string[] =
+      questionData?.answer_type === "MCQ"
+        ? questionData?.answers.split(/,\s*/) //gets rid of the whitespace
+        : [questionData?.answers];
+    setQuestionAnswers(answer_);
+
+    if (questionData?.answer_type === "MCQ") {
+      //Fisher-Yates Shuffle (random shuffle)
+      const shuffledArray: number[] = [...randomQuestionIndexes];
+      for (let i = shuffledArray.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+
+        [shuffledArray[i], shuffledArray[j]] = [
+          shuffledArray[j],
+          shuffledArray[i],
+        ];
+        i;
+      }
+      setRandomQuestionIndexes(shuffledArray);
+    }
+  }, [questionData]);
+
+  useEffect(() => {
+    const newArr: string[] = [
+      questionAnswers[randomQuestionIndexes[0]],
+      questionAnswers[randomQuestionIndexes[1]],
+      questionAnswers[randomQuestionIndexes[2]],
+      questionAnswers[randomQuestionIndexes[3]],
+    ];
+
+    setShuffledAnswers([...newArr]);
+  }, [randomQuestionIndexes]);
+
+  useEffect(() => {
+    if (
+      questionData?.answer_type === "FRQ" ||
+      questionData?.answer_type === "True/False"
+    ) {
+      if (finalAnswer === questionAnswers[0]) {
+        setTotalAnswered((prev) => prev + 1);
+        setTotalCorrect((prev) => prev + 1);
+      } else {
+        setTotalAnswered((prev) => prev + 1);
+      }
+    } else if (questionData?.answer_type === "MCQ") {
+      if (finalAnswer === shuffledAnswers[randomQuestionIndexes.indexOf(0)]) {
+        setTotalAnswered((prev) => prev + 1);
+        setTotalCorrect((prev) => prev + 1);
+      } else {
+        setTotalAnswered((prev) => prev + 1);
+      }
+    }
+  }, [finalAnswer]);
+
   return (
     <div
       className={`absolute p-[3px] bg-linear-to-r from-[#026640] via-[#0c3c64] to-[#151287] rounded-lg h-[80dvh] w-[80dvw] portrait:min-w-60 portrait:min-h-10 portrait:max-w-120 portrait:max-h-120 landscape:min-h-60 landscape:min-w-10 landscape:max-h-120 landscape:max-w-120 ${questionsVisible ? "scale-100 opacity-100" : "scale-0 opacity-0"}`}
     >
       <div className="flex flex-col w-full h-full bg-black justify-center items-center rounded-lg p-10 gap-5">
-        <div className="flex flex-row w-full justify-center items-center">
-          <h1 className="font-bold text-2xl">{questionData?.question}</h1>
-        </div>
-        <div className="flex flex-col w-full justify-center items-center"></div>
+        {!endTrivia && (
+          <>
+            <div className="flex flex-row w-full justify-center items-center">
+              <h1 className="font-bold text-2xl text-center">
+                {questionData?.question}
+              </h1>
+            </div>
+            {questionData?.answer_type === "MCQ" && (
+              <div className="flex flex-col w-fit justify-start items-center gap-5">
+                <div className="flex flex-row w-full justify-start items-center gap-8">
+                  <button
+                    onClick={() => {
+                      setSelectedLetter("A");
+                      setFinalAnswer(shuffledAnswers[0]);
+                      setShuffleQuestion((prev) => prev + 1);
+                    }}
+                    className="group flex rounded-full items-center justify-center h-10 w-10 min-w-10 bg-linear-to-r from-[#026640] via-[#0c3c64] to-[#151287] p-[3px] text-base hover:shadow-lg hover:shadow-[#110e73]/30 transition ease-in-out duration:300"
+                  >
+                    <div
+                      className={`rounded-full group-hover:bg-[#111111] ${selectedLetter === "A" ? "bg-[#1e1e1e]" : "bg-black"} transition ease-in-out duration:300 h-full w-full flex justify-center items-center group-hover:transition group-hover:ease-in-out group-hover:duration-300`}
+                    >
+                      A
+                    </div>
+                  </button>
+                  <p className="w-full max-w-60">{shuffledAnswers[0]}</p>
+                </div>
+                <div className="flex flex-row w-full justify-start items-center gap-8">
+                  <button
+                    onClick={() => {
+                      setSelectedLetter("B");
+                      setFinalAnswer(shuffledAnswers[1]);
+                      setShuffleQuestion((prev) => prev + 1);
+                    }}
+                    className="group flex rounded-full items-center justify-center h-10 w-10 min-w-10 bg-linear-to-r from-[#026640] via-[#0c3c64] to-[#151287] p-[3px] text-base hover:shadow-lg hover:shadow-[#110e73]/30 transition ease-in-out duration:300"
+                  >
+                    <div
+                      className={`rounded-full group-hover:bg-[#111111] ${selectedLetter === "B" ? "bg-[#1e1e1e]" : "bg-black"} transition ease-in-out duration:300 h-full w-full flex justify-center items-center group-hover:transition group-hover:ease-in-out group-hover:duration-300`}
+                    >
+                      B
+                    </div>
+                  </button>
+                  <p className="w-full max-w-60">{shuffledAnswers[1]}</p>
+                </div>
+                <div className="flex flex-row w-full justify-start items-center gap-8">
+                  <button
+                    onClick={() => {
+                      setSelectedLetter("C");
+                      setFinalAnswer(shuffledAnswers[2]);
+                      setShuffleQuestion((prev) => prev + 1);
+                    }}
+                    className="group flex rounded-full items-center justify-center h-10 w-10 min-w-10 bg-linear-to-r from-[#026640] via-[#0c3c64] to-[#151287] p-[3px] text-base hover:shadow-lg hover:shadow-[#110e73]/30 transition ease-in-out duration:300"
+                  >
+                    <div
+                      className={`rounded-full group-hover:bg-[#111111] ${selectedLetter === "C" ? "bg-[#1e1e1e]" : "bg-black"} transition ease-in-out duration:300 h-full w-full flex justify-center items-center group-hover:transition group-hover:ease-in-out group-hover:duration-300`}
+                    >
+                      C
+                    </div>
+                  </button>
+                  <p className="w-full max-w-60">{shuffledAnswers[2]}</p>
+                </div>
+                <div className="flex flex-row w-full justify-start items-center gap-8">
+                  <button
+                    onClick={() => {
+                      setSelectedLetter("D");
+                      setFinalAnswer(shuffledAnswers[3]);
+                      setShuffleQuestion((prev) => prev + 1);
+                    }}
+                    className="group flex rounded-full items-center justify-center h-10 w-10 min-w-10 bg-linear-to-r from-[#026640] via-[#0c3c64] to-[#151287] p-[3px] text-base hover:shadow-lg hover:shadow-[#110e73]/30 transition ease-in-out duration:300"
+                  >
+                    <div
+                      className={`rounded-full group-hover:bg-[#111111] ${selectedLetter === "D" ? "bg-[#1e1e1e]" : "bg-black"} transition ease-in-out duration:300 h-full w-full flex justify-center items-center group-hover:transition group-hover:ease-in-out group-hover:duration-300`}
+                    >
+                      D
+                    </div>
+                  </button>
+                  <p className="w-full max-w-60">{shuffledAnswers[3]}</p>
+                </div>
+                {/* <p className="text-sm/6 text-white/50">
+                  {totalCorrect}/{totalAnswered}
+                </p> */}
+              </div>
+            )}
+            {questionData?.answer_type === "FRQ" && (
+              <div className="flex flex-col w-full justify-center items-center">
+                <Field>
+                  {/* <Label className="text-sm/6 font-medium text-white">Answer</Label> */}
+                  <Description className="text-sm/6 text-white/50">
+                    Click enter to submit
+                  </Description>
+                  <Input
+                    onKeyDown={(e) => {
+                      if (e.key === "Enter") {
+                        setFinalAnswer(guessedAnswer);
+                        setGuessedAnswer("");
+                        setShuffleQuestion((prev) => prev + 1);
+                      }
+                    }}
+                    value={guessedAnswer}
+                    onChange={(e) => setGuessedAnswer(e.target.value)}
+                    className={clsx(
+                      "mt-3 block w-full rounded-lg border-none bg-white/5 px-3 py-1.5 text-sm/6 text-white",
+                      "focus:not-data-focus:outline-none data-focus:outline-2 data-focus:-outline-offset-2 data-focus:outline-white/25",
+                    )}
+                  />
+                </Field>
+                {/* <p className="text-sm/6 text-white/50">
+                  {totalCorrect}/{totalAnswered}
+                </p> */}
+              </div>
+            )}
+            {questionData?.answer_type === "True/False" && (
+              <div className="flex flex-col w-full items-center justify-center gap-5">
+                <div className="flex flex-row w-full justify-around items-center">
+                  <button
+                    onClick={() => {
+                      setSelectedLetter("True");
+                      setFinalAnswer("True");
+                      setShuffleQuestion((prev) => prev + 1);
+                    }}
+                    className="group flex rounded-full items-center justify-center h-10 w-20 bg-linear-to-r from-[#026640] via-[#0c3c64] to-[#151287] p-[3px] text-base hover:shadow-lg hover:shadow-[#110e73]/30 transition ease-in-out duration:300"
+                  >
+                    <div
+                      className={`rounded-full group-hover:bg-[#111111] ${selectedLetter === "True" ? "bg-[#1e1e1e]" : "bg-black"} transition ease-in-out duration:300 h-full w-full flex justify-center items-center group-hover:transition group-hover:ease-in-out group-hover:duration-300`}
+                    >
+                      True
+                    </div>
+                  </button>
+                  <button
+                    onClick={() => {
+                      setSelectedLetter("False");
+                      setFinalAnswer("False");
+                      setShuffleQuestion((prev) => prev + 1);
+                    }}
+                    className="group flex rounded-full items-center justify-center h-10 w-20 bg-linear-to-r from-[#026640] via-[#0c3c64] to-[#151287] p-[3px] text-base hover:shadow-lg hover:shadow-[#110e73]/30 transition ease-in-out duration:300"
+                  >
+                    <div
+                      className={`rounded-full group-hover:bg-[#111111] ${selectedLetter === "False" ? "bg-[#1e1e1e]" : "bg-black"} transition ease-in-out duration:300 h-full w-full flex justify-center items-center group-hover:transition group-hover:ease-in-out group-hover:duration-300`}
+                    >
+                      False
+                    </div>
+                  </button>
+                </div>
+              </div>
+            )}
+            <p className="text-sm/6 text-white/50">
+              {questionData?.difficulty} | {totalCorrect}/{totalAnswered}
+            </p>
+          </>
+        )}
+        {endTrivia && (
+          <div>
+            <p className="font-bold text-2xl">
+              You answered {totalCorrect}/{totalAnswered}.
+            </p>
+          </div>
+        )}
       </div>
     </div>
   );
