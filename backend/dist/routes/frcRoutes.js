03cff30fb0319a096c5295e1522a10abb9697f0d
Ansh Raghapur <anshraghapur@gmail.com>
2026-07-09 20:29:31 +0530

    trying one last ditch effort to use vercel serverless hosting

---
+++ backend/dist/routes/frcRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { getAreaData, getTeamData, } from "../controllers/wordle/frcWordleController.js";
+import { getTriviaQuestions } from "../controllers/trivia/frcTriviaController.js";
+export const router = express.Router();
+//wordle
+router.get("/wordle/team", getTeamData);
+router.get("/wordle/multiple", getAreaData);
+//trivia
+router.get("/trivia/get-questions", getTriviaQuestions);
---
+++ backend/dist/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseURL = process.env.SUPABASE_URL;
+const publishableKey = process.env.SUPABASE_PUBLISHABLE_KEY;
+const serviceKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+export const supabaseClient = createClient(supabaseURL, publishableKey);
+export const supabaseAdmin = createClient(supabaseURL, serviceKey);
