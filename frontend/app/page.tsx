5cfa349e81142e59239d4d4f67dce1c7ccb30fb1
Ansh Raghapur <anshraghapur@gmail.com>
2026-07-21 17:24:17 +0530

    fix: made the mode buttons more intuitive to click (sorry about that. they actually worked before, you just had to click on the word)

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -34,39 +34,39 @@ export default function Home() {
         className="mt-[dvh] flex flex-col p-5 justify-between gap-8"
       >
         <div className="w-fit flex flex-row">
-          <div
-            id="wordle"
-            className="group h-[20dvh] max-h-20 min-h-20 w-[45dvw] max-w-85 min-w-15 flex justify-center items-center rounded-full bg-linear-to-r from-[#026640] via-[#0c3c64] to-[#151287] p-[3px] mr-3 hover:shadow-lg hover:shadow-[#110e73]/30 transition ease-in-out duration:300"
-          >
-            <div className="rounded-full bg-black group-hover:bg-[#111111] transition ease-in-out duration:300 h-full w-full flex justify-center items-center group-hover:transition group-hover:ease-in-out group-hover:duration-300">
-              <Link href={`/${isToggled ? "frc" : "ftc"}/wordle`}>
+          <Link href={`/${isToggled ? "frc" : "ftc"}/wordle`}>
+            <div
+              id="wordle"
+              className="group h-[20dvh] max-h-20 min-h-20 w-[45dvw] max-w-85 min-w-15 flex justify-center items-center rounded-full bg-linear-to-r from-[#026640] via-[#0c3c64] to-[#151287] p-[3px] mr-3 hover:shadow-lg hover:shadow-[#110e73]/30 transition ease-in-out duration:300"
+            >
+              <div className="rounded-full bg-black group-hover:bg-[#111111] transition ease-in-out duration:300 h-full w-full flex justify-center items-center group-hover:transition group-hover:ease-in-out group-hover:duration-300">
                 <p className="text-2xl w-fit">Wordle</p>
-              </Link>
+              </div>
             </div>
-          </div>
-          <div
-            id="trivia"
-            className="group h-[20dvh] max-h-20 min-h-20 w-[45dvw] max-w-85 min-w-15 flex justify-center items-center rounded-full bg-linear-to-r from-[#026640] via-[#0c3c64] to-[#151287] p-[3px] ml-3 hover:shadow-lg hover:shadow-[#110e73]/30 transition ease-in-out duration:300"
-          >
-            <div className="rounded-full bg-black group-hover:bg-[#111111] transition ease-in-out duration:300 h-full w-full flex justify-center items-center group-hover:transition group-hover:ease-in-out group-hover:duration-300">
-              <Link href={`/${isToggled ? "frc" : "ftc"}/trivia`}>
+          </Link>
+          <Link href={`/${isToggled ? "frc" : "ftc"}/trivia`}>
+            <div
+              id="trivia"
+              className="group h-[20dvh] max-h-20 min-h-20 w-[45dvw] max-w-85 min-w-15 flex justify-center items-center rounded-full bg-linear-to-r from-[#026640] via-[#0c3c64] to-[#151287] p-[3px] ml-3 hover:shadow-lg hover:shadow-[#110e73]/30 transition ease-in-out duration:300"
+            >
+              <div className="rounded-full bg-black group-hover:bg-[#111111] transition ease-in-out duration:300 h-full w-full flex justify-center items-center group-hover:transition group-hover:ease-in-out group-hover:duration-300">
                 <p className="text-2xl w-fit">Trivia</p>
-              </Link>
+              </div>
             </div>
-          </div>
+          </Link>
         </div>
-        <div className="w-full flex flex-row">
-          <div
-            id="clicker"
-            className="group h-[20dvh] max-h-20 min-h-20 w-full min-w-15 flex justify-center items-center rounded-full bg-linear-to-r from-[#026640] via-[#0c3c64] to-[#151287] p-[3px] hover:shadow-lg hover:shadow-[#110e73]/30 transition ease-in-out duration:300"
-          >
-            <div className="rounded-full bg-black group-hover:bg-[#111111] transition ease-in-out duration:300 h-full w-full flex justify-center items-center group-hover:transition group-hover:ease-in-out group-hover:duration-300">
-              <Link href={`/${isToggled ? "frc" : "ftc"}/clicker`}>
+        <Link href={`/${isToggled ? "frc" : "ftc"}/clicker`}>
+          <div className="w-full flex flex-row">
+            <div
+              id="clicker"
+              className="group h-[20dvh] max-h-20 min-h-20 w-full min-w-15 flex justify-center items-center rounded-full bg-linear-to-r from-[#026640] via-[#0c3c64] to-[#151287] p-[3px] hover:shadow-lg hover:shadow-[#110e73]/30 transition ease-in-out duration:300"
+            >
+              <div className="rounded-full bg-black group-hover:bg-[#111111] transition ease-in-out duration:300 h-full w-full flex justify-center items-center group-hover:transition group-hover:ease-in-out group-hover:duration-300">
                 <p className="text-2xl w-fit">Clicker</p>
-              </Link>
+              </div>
             </div>
           </div>
-        </div>
+        </Link>
       </div>
     </main>
   );
